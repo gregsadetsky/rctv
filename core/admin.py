a5ce3463b52550225c60103f9c23beabb2543ea1
gregsadetsky <lepetitg@gmail.com>
2023-11-07 21:31:47 -0500

    .

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import App, User
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(App)
