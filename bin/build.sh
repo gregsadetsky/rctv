2abee0f08be66c99afe048ff90ae135acaa7d183
gregsadetsky <lepetitg@gmail.com>
2023-11-08 11:32:34 -0500

    .

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # 'compile' sdk using bun
-(cd sdk && npm install bun && bun install && bun bundle)
+(cd sdk && npm install bun && ./node_modules/bun/bin/bun install && ./node_modules/bun/bin/bun bundle)
 
 pip install --upgrade pippip install --force-reinstall -U setuptools
 pip install -r requirements.txt
