0632f0d7a5265826dbf9e2c3ed50275031a96919
gregsadetsky <lepetitg@gmail.com>
2025-03-16 20:43:37 -0400

    .

--- core/admin.py
+++ core/admin.py
@@ -8,7 +8,7 @@ admin.site.register(User, UserAdmin)
 
 class AppAdmin(admin.ModelAdmin):
     list_display = ("url", "created_at", "enabled")
-    actions = ["disable_apps"]
+    actions = ["disable_apps", "enable_apps"]
 
     def disable_apps(self, request, queryset):
         updated = queryset.update(enabled=False)
@@ -18,6 +18,15 @@ class AppAdmin(admin.ModelAdmin):
         )
 
     disable_apps.short_description = "Disable selected apps"
+
+    def enable_apps(self, request, queryset):
+        updated = queryset.update(enabled=True)
+        self.message_user(
+            request,
+            f"{updated} {'app was' if updated == 1 else 'apps were'} successfully enabled.",
+        )
+
+    enable_apps.short_description = "Enable selected apps"
 
 
 admin.site.register(App, AppAdmin)
