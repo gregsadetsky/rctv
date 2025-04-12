f0674ce9250c323fa7e292dd6538e72dd360b33b
gregsadetsky <lepetitg@gmail.com>
2025-04-12 13:17:11 -0400

    .

--- bin/serve.sh
+++ bin/serve.sh
@@ -2,4 +2,4 @@
 # exit on error
 set -o errexit
 
-gunicorn --access-logformat '%(h)s %(l)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" reqtime: %(M)s ms' rctv.wsgi:application
+gunicorn -b 0.0.0.0:8000 --access-logformat '%(h)s %(l)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" reqtime: %(M)s ms' rctv.wsgi:application
