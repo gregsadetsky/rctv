9c5e72f54701b6e65ea20d2ac263419fa773ade7
Josh Wolfe <thejoshwolfe@gmail.com>
2025-11-14 13:55:50 -0500

    revert changes to wsgi

--- rctv/wsgi.py
+++ rctv/wsgi.py
@@ -9,13 +9,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
-if "DJANGO_SETTINGS_MODULE" not in os.environ:
-    import sys, time
-    print("ERROR: you must set DJANGO_SETTINGS_MODULE=rctv.settings.dev (or other) before running the server", file=sys.stderr)
-    print("ERROR: (Ctrl+C to exit and solve this problem.)", file=sys.stderr)
-    while True:
-        time.sleep(999)
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rctv.settings")
 
 application = get_wsgi_application()
